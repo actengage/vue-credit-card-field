c57e126fe47ce130c2e4c73ff6f79e5b305c3abf
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2019-01-23 11:07:08 -0700

    0.2.4

--- src/icons.js
+++ src/icons.js
@@ -1,13 +1,13 @@
 // Load the icons
 import { faCcJcb } from '@fortawesome/free-brands-svg-icons/faCcJcb';
 import { faCcVisa } from '@fortawesome/free-brands-svg-icons/faCcVisa';
-import { faCcAmex } from '@fortawesome/free-brands-svg-icons/faCCAmex';
+import { faCcAmex } from '@fortawesome/free-brands-svg-icons/faCcAmex';
 import { faCcDiscover } from '@fortawesome/free-brands-svg-icons/faCcDiscover';
 import { faCcMastercard } from '@fortawesome/free-brands-svg-icons/faCcMastercard';
 import { faCcDinersClub } from '@fortawesome/free-brands-svg-icons/faCcDinersClub';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons/faExclamationTriangle';
-import { faCreditCard as faCreditCardFront } from '@fortawesome/free-regular-svg-icons/faCreditCard';
 import { faCreditCard as faCreditCardBack } from '@fortawesome/free-solid-svg-icons/faCreditCard';
+import { faCreditCard as faCreditCardFront } from '@fortawesome/free-regular-svg-icons/faCreditCard';
 
 // Load the font awesome library
 import { library } from '@fortawesome/fontawesome-svg-core';
