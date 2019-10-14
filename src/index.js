1b9132236c575073b14f804b3f29ea08c1a750ee
Aniefon Umanah <31783702+Anietex@users.noreply.github.com>
2019-10-14 20:14:32 +0100

    Fixed exporting and an object instead of exporting individual component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import CreditCardField from './Components/CreditCardField';
 import InlineCreditCardField from './Components/InlineCreditCardField';
 
-export default {
+export {
     CreditCardField,
     InlineCreditCardField
 };
